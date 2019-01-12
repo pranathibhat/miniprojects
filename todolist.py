fe9a702069d8d0532dee34c6ff6891753da50e53
Your Name <pranathibhat97@gmail.com>
2019-01-12 23:38:08 +0530

    Documented

--- todolist.py
+++ todolist.py
@@ -2,12 +2,20 @@ to_do=[]
 done=[]
 class ToDoList:
 
+    """A class that handles the task of a to-do list"""
+
     def add():
-            print()
-            x=input("Enter the task to add : ")
-            to_do.append(x)
-         
+        
+
+        """A  function that adds the tasks to list"""
+        print()
+        x=input("Enter the task to add : ")
+        to_do.append(x)
+	     
     def mark_done():
+        """A function that marks the task as done when the user says the particular
+            task is done"""
+        
         print()
         y=input("Enter the task done : ")
         if y in to_do:
@@ -17,41 +25,40 @@ class ToDoList:
             print("Enter the task in the list")
 
     def task_rem():
+        """A function that returns the user with a list of things yet to be done"""
+        
         print()
         print('The tasks remaining are : ')
-        if to_do==[]:
-            print("There are no tasks to be done")
-        else:
-             y=[i for i in to_do]
-             print(y)
+        for z in to_do:
+            print(z)
 
     def task_done():
+        """A function that returns the user with a list of tasks that are done"""
+        
         print()
         print('The tasks completed are : ')
-        if done==[]:
-            print("There are no tasks done")
-        else:
-             y=[i for i in done]
-             print(y)
+        for z in done:
+            print(z)
 
 def main():
-    
- print()
- while True:
 
-    print("1.Add task  2.Mark done  3.Tasks remaining  4.Tasks done  5.exit")
-    opt=int(input("Enter option : "))
-    if opt==1:
-        ToDoList.add()
-    elif opt==2:
-        ToDoList.mark_done()
-    elif opt==3:
-        ToDoList.task_rem()
-    elif opt==4:
-        ToDoList.task_done()
-    else:
-        break
+    """Asking user for the input for the functions defined above"""
+    
+    print()
+    while True:
+
+        print("1.Add task  2.Mark done  3.Tasks remaining  4.Tasks done  5.exit")
+        opt=int(input("Enter option : "))
+        if opt==1:
+            ToDoList.add()
+        elif opt==2:
+            ToDoList.mark_done()
+        elif opt==3:
+            ToDoList.task_rem()
+        elif opt==4:
+            ToDoList.task_done()
+        else:
+            break
 
 
 main()
-
