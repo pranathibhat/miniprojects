5b0959d84436a1ae2789359996b55b71d46ccb00
Your Name <pranathibhat97@gmail.com>
2019-01-12 23:19:52 +0530

    2nd commit

--- meeting.py
+++ meeting.py
@@ -1,3 +1,23 @@
 class Meeting:
 
-    def __init__(self,dictionary_name
+    def __init__(self,key):
+    	
+    	self.dictionary_name={1:"9 am",2:"11 am",3:"12.30pm",4:"3pm",5:"7pm",6:"9pm"}
+    	self.key=key
+    	self.new_dic={}
+
+    def time_view(self):
+    	mine=int(input())
+    	for i in self.dictionary_name.keys():
+            if(mine==i):
+                self.new_dic.update({i:self.dictionary_name[i]})
+                del self.dictionary_name[i]
+    	    else:
+    		print("Enter proper time slot")
+    	return self.new_dic
+
+
+a=Meeting(3)
+b=a.time_view()
+print(b)
+
