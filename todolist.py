14fae3595c74dfa782bf529d146c0a6c53f6cbd8
Your Name <you@example.com>
2019-01-12 17:26:15 +0530

    To do list

---
+++ todolist.py
@@ -0,0 +1,34 @@
+class Todo:
+
+    def __init__(self,list_name,to_do,done):
+
+        self.list_name=list_name
+        self.to_do=to_do
+        self.done=done
+
+    def add(self):
+
+        self.ToDoList=[]
+        self.list_name=input("Enter the list name:")
+        self.n=int(input("Enter the number of tasks:")
+        for i in range(self.n):
+            self.Task=input("Enter the task:")
+            self.ToDoList.append(Task)
+        return self.ToDoList
+
+    def mark_done(self):
+
+        self.done=input("Enter the task that is done:")
+
+        for i in self.ToDoList:
+
+            if i==self.done:
+
+                
+            
+        
+
+
+
+
+        
